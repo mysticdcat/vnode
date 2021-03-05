35d0c9e571e926c9f50802d4542ce24b2efe34ae
DESKTOP-DK128U1\admin <zouzhijin@gongzhugou.com.cn>
2021-03-05 09:46:13 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import h from "./h";
 import patch from "./patch";
-let vnode1 = h('ul',{},[
-    h('li',{},'son1'),
-    h('li',{},'son2')
+let vnode1 = h('ul', {}, [
+    h('li', { key: 'A' }, 'A'),
+    h('li', { key: 'B' }, 'B'),
+    h('li', { key: 'C' }, 'C'),
+    h('li', { key: 'D' }, 'D'),
+    h('li', { key: 'E' }, 'E')
 ])
 const container = document.getElementById('container')
-patch(container,vnode1)
-let vnode2 = h('ol',{},[
-    h('li',{},'儿子'),
-    h('li',{},'女儿')
+patch(container, vnode1)
+let vnode2 = h('ul', {}, [
+    h('li', { key: 'Q' }, 'Q'),
+    h('li', { key: 'T' }, 'T'),
+    h('li', { key: 'A' }, 'A'),
+    h('li', { key: 'B' }, 'B'),
+    h('li', { key: 'Z' }, 'Z'),
+    h('li', { key: 'C' }, 'C'),
+    h('li', { key: 'D' }, 'D'),
+    h('li', { key: 'E' }, 'E')
 ])
 
 let button = document.querySelector('.change-button')
 console.log(button)
-button.addEventListener('click',() => {
-    patch(vnode1,vnode2)
+button.addEventListener('click', () => {
+    patch(vnode1, vnode2)
 })
--- src/pacthVnode.js
+++ src/pacthVnode.js
@@ -1,5 +1,31 @@
-export default function pacthVnode (oldVnode,newVnode) {
+import createElement from "./createElement";
+import updateChildren from "./updateChildren"
+export default function pacthVnode(oldVnode, newVnode) {
     // console.log(...arguments,'是一个节点')
     // // 
     //  if ()
+    // 我们规定children和text不允许同时出现
+    // 所以当出现text时   我们只需要直接替换文本值即可
+    if (newVnode.text) {
+        if (newVnode.text != oldVnode.text) {
+            // 如果新虚拟节点中的text和老的虚拟节点的text不同，那么直接让新的text写入老的elm中即可。如果老的elm中是children，那么也会立即消失掉。
+            oldVnode.elm.innerText = newVnode.text;
+        }
+    } else {
+        // 此时说明有children
+        // 判断老的有没有children
+        if (oldVnode.children != undefined && oldVnode.children.length > 0) {
+            // 老的有children，新的也有children，此时就是最复杂的情况。
+            updateChildren(oldVnode.elm, oldVnode.children, newVnode.children);
+        } else {
+             // 清空老的节点的内容
+             oldVnode.elm.innerHTML = '';
+            // 老的没有children   这时候我们直接创建
+            for (let i = 0; i < newVnode.children.length; i++) {
+                let dom = createElement(newVnode.children[i]);
+                oldVnode.elm.appendChild(dom);
+            }
+        }
+    }
+
 }
